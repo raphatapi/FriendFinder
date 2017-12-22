a0a14189767ee5a8b1569f448b05ca4a15496b2a
raphatapi <raphaeltapioca10@gmail.com>
2017-12-21 22:47:41 -0700

    survey

---
+++ app/data/friends.js
@@ -0,0 +1,17 @@
+var friendsList = [
+
+	{
+		name: "Jean Girard",
+		photo: "https://pbs.twimg.com/profile_images/378800000255469829/2f9851aca73208270206e05a4684d2df_400x400.jpeg",
+		scores: [2, 5, 1, 3, 2, 4, 1, 2, 1, 1]
+
+    },
+    {
+		name: "Bruno",
+		photo: "http://img.timeinc.net/time/daily/2009/0907/360_abruno_0720.jpg",
+		scores: [2, 5, 1, 3, 2, 4, 1, 2, 1, 1]
+
+    }
+];
+
+module.exports = friendsList;
