6522fe3e4e3924c14206e691abecdfb9aa95e603
raphatapi <raphaeltapioca10@gmail.com>
2017-12-26 23:36:03 -0700

    polish

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -25,7 +25,6 @@ module.exports = function(app) {
                 index = j;
             };
         };
-		// console.log('best Choice', friend[index]);
         friend.push(newClient);
         res.json(friend[index]);
 	});
